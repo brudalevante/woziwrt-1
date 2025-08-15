314577f72ed710c9835f1c243f9edb7e3652a152
brudalevante <continentalwl@gmail.com>
2025-08-15 21:59:26 +0200

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -54,7 +54,7 @@ echo "==== 7. COPIA LA CONFIGURACIÓN BASE (config_mm_06082025) ===="
 cp -v ../configs/config_mm_06082025 .config
 
 echo "==== 8. COPIA TU CONFIGURACIÓN PERSONALIZADA AL DEFCONFIG DEL AUTOBUILD ===="
-cp -v ../configs/config_mm_06082025../mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
+cp -v ../configs/config_mm_06082025 ../mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
 
 echo "==== 9. ACTUALIZA E INSTALA FEEDS ===="
 ./scripts/feeds update -a
